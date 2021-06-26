d718a4b22bcf1826f7362aed172ff0d3bb07870a
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-26 13:05:30 +0200

    Ajout de la solution pour le test

---
+++ nightwatch/first_e2e_test.js
@@ -0,0 +1,17 @@
+module.exports = {
+    test: client => {
+        client
+            .url('http://127.0.0.1:5500/')
+            .waitForElementVisible('.main-title', 10 * 1000)
+            .assert.visible('input[type=email]')
+            .setValue('input[type=email]', 'thomas@facadia.com')
+            .assert.visible('input[type=password]')
+            .setValue('input[type=password]', 'azerty')
+            .assert.visible('button[type=submit]')
+            .click('button[type=submit]')
+            .waitForElementVisible('.section-title', 10 * 1000)
+            .assert.containsText('.section-title', 'Vos capteurs')
+            .assert.visible('.sensor-info-btn')
+            .click('.sensor-info-btn')
+    }
+}
