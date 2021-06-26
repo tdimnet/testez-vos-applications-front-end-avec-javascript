ded970906558bdd65d735b828cc05638253df10f
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-26 18:05:42 +0200

    Ajout des snippets de code pour la question 3d du quiz

---
+++ js/quiz/index.js
@@ -0,0 +1,31 @@
+const total = 10
+
+/**
+ * 
+ * @param {string} name 
+ * @returns {string}
+ */
+const sayHello = name => `hello, ${name}`
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+const makeSum = (a, b) => a + b
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+const makeProduct = (a, b) => total ? a * b : a * total
+
+/**
+ * 
+ * @param {number} age 
+ * @returns {string}
+ */
+const getAge = (age) => `${age} ans`
