032de2b9b3b78cd6117d769741062d98488ee81f
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-26 15:51:16 +0200

    Ajout de la question du test

--- nightwatch/quiz_test.js
+++ nightwatch/quiz_test.js
@@ -1,7 +1,16 @@
 module.exports = {
     test: client => {
         client
-            // Write your test here
             .url('http://127.0.0.1:5500/')
+            .waitForElementVisible('.main-title', 10 * 1000)
+            .assert.visible('input[type=email]')
+            .setValue('input[type=email]', 'thomas@facada.com')
+            .setValue('input[type=password]', 'azerty')
+            .click('button[type=submit]')
+            .assert.visible('.user-email-error-msg')
+            .setValue('input[type=email]', 'thomas@facadia.com')
+            .click('button[type=submit]')
+            .assert.not.visible('.user-email-error-msg')
+            .end()
     }
 }
