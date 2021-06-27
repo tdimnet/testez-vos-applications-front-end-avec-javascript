262a4fd0d46fb07cb7febcd74a8f44d444fb60bb
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-27 15:35:44 +0200

    Préparation des deux cas de test

--- js/utils/api/sensorsApi.test.js
+++
@@ -1,8 +0,0 @@
-import { data } from '../../../data/mock-homepage-data'
-import { retrieveSensorsData } from './sensorsApi'
-
-describe('Sensors API Unit Test Suites', () => {
-    it('should return the mocked data', () => {
-        expect(retrieveSensorsData()).toBe(data.facades)
-    })
-})
