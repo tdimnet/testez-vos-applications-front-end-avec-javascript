60746f384c46bb7135d3c307c7f0825044e5d0c2
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-27 15:56:42 +0200

    Ajout d'un deuxième test d'exemple

--- js/utils/env/index.test.js
+++ js/utils/env/index.test.js
@@ -5,4 +5,9 @@ describe('Env Utils Unit Test Suite', () => {
     it('should be in test env', () => {
         expect(isInTestEnv()).toBe(true)
     })
+    
+    it('should not be in test env', () => {
+        process.env.NODE_ENV = 'dev'
+        expect(isInTestEnv()).not.toBe(true)
+    })
 })
