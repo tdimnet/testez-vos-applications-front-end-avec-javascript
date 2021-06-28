733d13a5a4534f856e82060e82ac188e6ccfca8d
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-06-28 14:21:16 +0200

    Fix du bug de l'API

--- js/utils/api/sensorsApi.js
+++ js/utils/api/sensorsApi.js
@@ -4,7 +4,7 @@ import { isInTestEnv } from '../env/index.js'
 
 export const retrieveSensorsData = () => isInTestEnv()
     ? data.facades
-    : fetch('http://localhost:5501/data/homepage-data.json')
+    : fetch('http://localhost:5500/data/homepage-data.json')
         .then(res => res.json())
         .then(data => data.facades)
         .catch(err => console.log("Oh no", err))
