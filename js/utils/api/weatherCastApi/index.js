982fcc82eaf0b43c4e9ca2fb6d0e40cade638964
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-07-12 09:19:06 +0200

    Remove API weathercast: still have some https issue

--- js/utils/api/weatherCastApi/index.js
+++ js/utils/api/weatherCastApi/index.js
@@ -1,18 +1,7 @@
 import { isInTestEnv } from '../../env/index.js'
 import { data } from '../../../../data/mock-weather-api-data.js'
 
-// Penser bien à ajouter votre clé d'API
-const ACCESS_KEY = '931f119b9236cc4743d277b1e73b6e77'
-
-const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
-    .then(res => res.json())
-    .catch(err => console.log("Oh no", err))
-
 
 export const retrieveWeatherForecastData = async (coordinates) => {
-    if (isInTestEnv()) {
-        return data
-    }
-
-    return await _retrieveWeatherForecastApiData(coordinates)
+    return data
 }
